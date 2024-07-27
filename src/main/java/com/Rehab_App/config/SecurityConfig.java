de746c2b482978dc14c4225de79c0ac23e859cca
veneva <veneva@imbm.bas.bg>
2024-07-27 21:00:59 +0300

    migrate to SDK22

--- src/main/java/com/Rehab_App/config/SecurityConfig.java
+++ src/main/java/com/Rehab_App/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class   SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
